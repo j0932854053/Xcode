fbd8f00b8c19779d9900e96035043f3a3002e139
loius_yuyu <j0932854053@gmail.com>
2024-01-08 21:58:38 +0800

    test Xcode with git

--- Leetcode/0804/0804/main.c
+++ Leetcode/0804/0804/main.c
@@ -6,21 +6,96 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+int *calLps(char *pat);
 
-struct Tree{
-    int val;
-    
-    struct Tree *left;
-    struct Tree *righ;
-    
-};
+bool kmpSearch(char *pat, char *str)
+{
+    int pLen = strlen(pat);
+    int sLen = strlen(str);
+    int *lps = calLps(pat);
+    int ipat = 0, istr = 0;
 
-
-
-
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    while (istr < sLen)
+    {
+        if (pat[ipat] == str[istr])
+        {
+            ++ipat;
+            ++istr;
+            if (ipat == pLen)
+            {
+                //printf("index : %d\n", istr - ipat);
+                return true;
+            }
+        }
+        else
+        {
+            if (ipat == 0)
+                ++istr;
+            else
+                ipat = lps[ipat - 1];
+        }
+    }
+    free(lps);
+    return false;
 }
+
+int *calLps(char *pat)
+{
+    int pLen = strlen(pat);
+    int *lps = (int *)malloc(sizeof(int) * pLen);
+    int ipat = 1, iwhole = 0;
+    lps[0] = 0;
+    while (ipat < pLen)
+    {
+        if (pat[ipat] == pat[iwhole])
+        {
+            lps[ipat++] = ++iwhole;
+        }
+        else if (iwhole == 0)
+        {
+            lps[ipat++] = iwhole;
+        }
+        else
+        {
+            iwhole = lps[iwhole - 1];
+        }
+    }
+    return lps;
+}
+
+int main(void)
+{
+    char *p = "aa";
+    // scanf("%s", p);
+
+    char *s[] = {
+        "aaaabbbbbb",
+        "aabcddddd",
+        "zabcd",
+        "aaghhh",
+        "abcdefg",
+        "xyzabc",
+        "xxxxxaaxxtwgr"};
+
+    int s_len = sizeof(s) / sizeof(s[0]);
+    for (int i = 0; i < s_len; i++)
+    {
+        
+        if (!kmpSearch(p, s[i])) {
+            printf("%s\n", s[i]);
+        }
+        
+    }
+
+    // printf("%lu\n", strlen(p));
+    // int *lps = calLps(p);
+    // for (int i = 0; i < strlen(p); i++)
+    // {
+    //     printf("%d\t", lps[i]);
+    // }
+    // printf("\n");
+}
+
---
+++ Leetcode/Container_With_Most_Water/Container_With_Most_Water/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Container_With_Most_Water
+//
+//  Created by 龔星宇 on 2024/1/8.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("init\n");
+    printf("Hello, World!\n");
+    return 0;
+}
