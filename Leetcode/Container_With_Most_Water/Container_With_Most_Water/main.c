898d7a7731eb9dbb05fea0db9bfdb36ebe30dc3e
loius_yuyu <j0932854053@gmail.com>
2024-01-08 23:47:30 +0800

    finish but didn't add comment

--- Leetcode/Container_With_Most_Water/Container_With_Most_Water/main.c
+++ Leetcode/Container_With_Most_Water/Container_With_Most_Water/main.c
@@ -6,10 +6,35 @@
 //
 
 #include <stdio.h>
+#define my_max(a, b) (((a) > (b)) ? (a) : (b))
+#define my_min(a, b) (((a) < (b)) ? (a) : (b))
+
+int maxArea(int* height, int heightSize) {
+    int maxarea = 0;
+    int l = 0;
+    int r = heightSize - 1;
+    while (l<r) {
+        maxarea = my_max(maxarea, my_min(height[l], height[r])*(r-l));
+        if(height[l]<height[r]){
+            l++;
+        }else{
+            r--;
+        }
+    }
+    
+    
+    return maxarea;
+}
+
 
 int main(int argc, const char * argv[]) {
     printf("init\n");
-    printf("test2\n");
-    printf("Hello, World!\n");
+    int height[] = {1,8,6,2,5,4,8,3,7};
+//    int height[] = {1,1};
+    int heightSize = sizeof(height)/sizeof(height[0]);
+    int result = maxArea(height, heightSize);
+    
+    printf("Max area is : %d \n", result);
+    
     return 0;
 }
